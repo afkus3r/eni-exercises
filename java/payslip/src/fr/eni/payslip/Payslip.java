30552df1cab81b0bdf8dbf1650ad99bddef7c54f
afkus3r <sheila.skalli@gmail.com>
2022-06-13 23:27:46 +0200

    Add main method program

--- java/payslip/src/fr/eni/payslip/Payslip.java
+++ java/payslip/src/fr/eni/payslip/Payslip.java
@@ -1,8 +1,10 @@
 package fr.eni.payslip;
 
+import java.util.Scanner;
+
 public class Payslip {
-    
-    public int calculateBaseSalary (int nbHours, int hourlyRate) {
+
+    static float calculateBaseSalary(int nbHours, float hourlyRate) {
         int baseSalary = 0;
         for (int i = 0; i < nbHours; i++) {
             if (i < 169) {
@@ -16,7 +18,7 @@ public class Payslip {
         return baseSalary;
     }
 
-    public float calculateContributions (int baseSalary) {
+    static float calculateContributions(float baseSalary) {
         float contributions = 0;
         System.out.println("Remboursement de la dette sociale : " + baseSalary * 0.0349);
         contributions += baseSalary * 0.0349;
@@ -35,7 +37,7 @@ public class Payslip {
         return contributions;
     }
 
-    public int calculateBonus (int nbChildren) {
+    static int calculateBonus(int nbChildren) {
         int bonus = 0;
         if (nbChildren == 1) {
             bonus = 20;
@@ -48,6 +50,27 @@ public class Payslip {
     }
 
     public static void main(String[] args) {
+        Scanner intInput = new Scanner(System.in);
+        Scanner floatInput = new Scanner(System.in);
 
+        System.out.println("Veuillez entrer le nombre d'heures travaillées.");
+        int nbHours = intInput.nextInt();
+
+        System.out.println("Veuillez entrer votre salaire horaire.");
+        float hourlyRate = floatInput.nextFloat();
+
+        System.out.println("Veuillez entrer le nombre d'enfants à charge.");
+        int nbChildren = intInput.nextInt();
+
+        float baseSalary = calculateBaseSalary(nbHours, hourlyRate);
+        System.out.println("Salaire mensuel de base : " + baseSalary);
+
+        float contributions = calculateContributions(baseSalary);
+        System.out.println("Cotisations sociales : " + contributions);
+
+        int bonus = calculateBonus(nbChildren);
+        System.out.println("Primes famille : " + bonus);
+
+        System.out.println("Net à payer : " + ((baseSalary + bonus) - contributions));
     }
 }
