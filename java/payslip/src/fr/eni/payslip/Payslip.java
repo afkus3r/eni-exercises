8da7c2b72fbb73236b83afa08abd8d19447ec62b
afkus3r <sheila.skalli@gmail.com>
2022-06-13 22:53:17 +0200

    Create calculateContributions function

--- java/payslip/src/fr/eni/payslip/Payslip.java
+++ java/payslip/src/fr/eni/payslip/Payslip.java
@@ -1,9 +1,8 @@
 package fr.eni.payslip;
 
 public class Payslip {
-    int baseSalary;
-
     public int calculateBaseSalary (int nbHours, int hourlyRate) {
+        int baseSalary = 0;
         for (int i = 0; i < nbHours; i++) {
             if (i < 169) {
                 baseSalary += hourlyRate;
@@ -14,6 +13,29 @@ public class Payslip {
             }
         }
         return baseSalary;
+    }
+
+    public float calculateContributions (int baseSalary) {
+        float contributions = 0;
+        System.out.println("Remboursement de la dette sociale : " + baseSalary * 0.0349);
+        contributions += baseSalary * 0.0349;
+        System.out.println("Contribution sociale généralisée : " + baseSalary * 0.0615);
+        contributions += baseSalary * 0.0615;
+        System.out.println("Assurance maladie : " + baseSalary * 0.0095);
+        contributions += baseSalary * 0.0095;
+        System.out.println("Assurance vieillesse : " + baseSalary * 0.0844);
+        contributions += baseSalary * 0.0844;
+        System.out.println("Assurance chômage : " + baseSalary * 0.0305);
+        contributions += baseSalary * 0.0305;
+        System.out.println("Retraite complémentaire : " + baseSalary * 0.0381);
+        contributions += baseSalary * 0.0381;
+        System.out.println("Cotisation AGFF : " + baseSalary * 0.0102);
+        contributions += baseSalary * 0.0102;
+        return contributions;
+    }
+
+    public int calculateBonus (int nbChildren) {
+        int bonus = 0;
     }
 
     public static void main(String[] args) {
