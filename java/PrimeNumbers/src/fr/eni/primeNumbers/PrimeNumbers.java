4d0035a920e219a0de4ed19fe59a57f0c763851a
afkus3r <sheila.skalli@gmail.com>
2022-06-14 15:53:47 +0200

    Create testPrimeNumbers function

--- java/PrimeNumbers/src/fr/eni/primeNumbers/PrimeNumbers.java
+++ java/PrimeNumbers/src/fr/eni/primeNumbers/PrimeNumbers.java
@@ -1,4 +1,22 @@
 package fr.eni.primeNumbers;
 
+import java.util.Scanner;
+
 public class PrimeNumbers {
+    static void testPrimeNumber(int num) {
+        boolean flag = false;
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) {
+            System.out.println(num);
+        }
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
