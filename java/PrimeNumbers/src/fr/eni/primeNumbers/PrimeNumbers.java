155ca1bc72b4e1583291b709929e2d6133f22c35
afkus3r <sheila.skalli@gmail.com>
2022-06-14 16:00:45 +0200

    Add display of the prime numbers

--- java/PrimeNumbers/src/fr/eni/primeNumbers/PrimeNumbers.java
+++ java/PrimeNumbers/src/fr/eni/primeNumbers/PrimeNumbers.java
@@ -17,6 +17,13 @@ public class PrimeNumbers {
     }
 
     public static void main(String[] args) {
-        
+        Scanner intInput = new Scanner(System.in);
+        System.out.println("Entrez un nombre jusqu'auquel vous voulez tester les nombres entiers.");
+        int numberInput = intInput.nextInt();
+
+        System.out.println("Voici les nombres premier jusqu'à " + numberInput + " :");
+        for (int i = 2; i < numberInput; i++) {
+            testPrimeNumber(i);
+        }
     }
 }
