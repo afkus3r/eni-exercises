5882ecac4ecc0c02ce8d1840f98e4ed361f1b527
afkus3r <sheila.skalli@gmail.com>
2022-07-11 23:33:59 +0200

    Create dictionary words array

--- java/Scrabble/src/fr/eni/Scrabble/Scrabble.java
+++ java/Scrabble/src/fr/eni/Scrabble/Scrabble.java
@@ -1,7 +1,29 @@
 package fr.eni.Scrabble;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Scrabble {
-    public static void main(String[] args){
-        
+    static List<String> dictionaryWords = new ArrayList<String>();
+    static int randomNumber = 0;
+    static char[] randomWord = new char[20];
+    static char[] shuffledLetters = new char[20];
+    static boolean isLettersVerified;
+    static boolean isWordVerified;
+
+    public static void main(String[] args) {
+
+    }
+
+    static String[] createDictionaryArray() throws FileNotFoundException {
+        FileInputStream file = new FileInputStream("./dictionnaire.txt");
+        Scanner fileScan = new Scanner(file);
+        while (fileScan.hasNextLine()) {
+            dictionaryWords.add(fileScan.nextLine());
+        }
+        return dictionaryWords.toArray(new String[0]);
     }
 }
