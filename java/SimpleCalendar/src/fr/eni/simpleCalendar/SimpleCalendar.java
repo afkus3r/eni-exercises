66c18f723b25217b0c6f190f3e6ed574bb58a179
afkus3r <sheila.skalli@gmail.com>
2022-07-16 23:18:30 +0200

    Create project

---
+++ java/SimpleCalendar/src/fr/eni/simpleCalendar/SimpleCalendar.java
@@ -0,0 +1,10 @@
+package fr.eni.simpleCalendar;
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class SimpleCalendar {
+    public static void main (String[] args) {
+
+    }
+}
