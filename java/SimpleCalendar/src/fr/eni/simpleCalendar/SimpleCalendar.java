934d1f35b1e56f156a7ab221d4fffe0c6d36549f
afkus3r <sheila.skalli@gmail.com>
2022-07-18 12:27:42 +0200

    Add menu to get user choice

--- java/SimpleCalendar/src/fr/eni/simpleCalendar/SimpleCalendar.java
+++ java/SimpleCalendar/src/fr/eni/simpleCalendar/SimpleCalendar.java
@@ -3,10 +3,33 @@ package fr.eni.simpleCalendar;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Objects;
+import java.util.Scanner;
 
 public class SimpleCalendar {
     public static void main (String[] args) {
-        displayCalendar(5, 2022);
+        Scanner stringInput = new Scanner(System.in);
+        Scanner intInput = new Scanner(System.in);
+
+        // Display current month calendar
+        System.out.println("Hello, here is the current month calendar :");
+        GregorianCalendar currentCal = new GregorianCalendar();
+        displayCalendar(currentCal.get(Calendar.MONTH), currentCal.get(Calendar.YEAR));
+
+        System.out.println("Press enter to chose another month");
+        String enter = stringInput.nextLine();
+        while (Objects.equals(enter, "")) {
+            System.out.println("Enter the number of the month you want");
+            int monthNum = intInput.nextInt();
+            System.out.println("Enter the year of your choice");
+            int year = intInput.nextInt();
+            displayCalendar(monthNum - 1, year);
+            System.out.println("Press enter to chose another month or press Q to quit");
+            enter = stringInput.nextLine();
+        }
+        if (Objects.equals(enter, "Q")) {
+            System.out.println("Goodbye !");
+
+        }
 
     }
     public static void displayCalendar(int month, int year) {
@@ -40,7 +63,7 @@ public class SimpleCalendar {
         }
 
         // Initialize first row with days names
-        String[][] matrix = new String[6][7];
+        String[][] matrix = new String[7][7];
         matrix[0][0] = "Mo";
         matrix[0][1] = "Tu";
         matrix[0][2] = "We";
@@ -65,10 +88,13 @@ public class SimpleCalendar {
         for (int i = 2; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 matrix[i][j] = String.valueOf(holder);
-                holder++;
                 if (Objects.equals(matrix[i][j], String.valueOf(lastDay))) {
                     break;
                 }
+                holder++;
+            }
+            if (holder == lastDay) {
+                break;
             }
         }
 
@@ -77,7 +103,7 @@ public class SimpleCalendar {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[i].length; j++) {
                 if (matrix[i][j] == null) {
-                    System.out.println(" ");
+                    System.out.print(" ");
                 } else {
                     System.out.print(matrix[i][j] + " ");
                 }
