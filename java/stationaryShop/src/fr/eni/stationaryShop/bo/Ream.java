35bb201a52774465123aec653fce0f0cce1da015
afkus3r <sheila.skalli@gmail.com>
2022-07-27 15:41:30 +0200

    Create ream class

---
+++ java/stationaryShop/src/fr/eni/stationaryShop/bo/Ream.java
@@ -0,0 +1,31 @@
+package fr.eni.stationaryShop.bo;
+
+public class Ream extends Item{
+    private int weight;
+
+    // Builders
+    public Ream(int itemID, String brand, String ref, String designation, float unitaryPrice, int stockQty, int weight) {
+        super(itemID, brand, ref, designation, unitaryPrice, stockQty);
+        this.weight = weight;
+    }
+
+    public Ream(String brand, String ref, String designation, float unitaryPrice, int stockQty, int weight) {
+        super(brand, ref, designation, unitaryPrice, stockQty);
+        this.weight = weight;
+    }
+
+    // Getter and setter
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    // toString
+    @Override
+    public String toString() {
+        return super.toString() + "Ream [" + "weight=" + weight + ']';
+    }
+}
