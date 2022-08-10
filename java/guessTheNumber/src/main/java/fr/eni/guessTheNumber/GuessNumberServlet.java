fd6571eebd5ce826e855c1c718356d5813fa2a2d
afkus3r <sheila.skalli@gmail.com>
2022-08-10 13:55:05 +0200

    Change redirect response method

--- java/guessTheNumber/src/main/java/fr/eni/guessTheNumber/GuessNumberServlet.java
+++ java/guessTheNumber/src/main/java/fr/eni/guessTheNumber/GuessNumberServlet.java
@@ -1,5 +1,6 @@
 package fr.eni.guessTheNumber;
 
+import java.io.IOException;
 import java.util.Random;
 
 import jakarta.servlet.http.*;
@@ -8,18 +9,16 @@ import jakarta.servlet.annotation.*;
 @WebServlet(name = "guessNumberServlet", value = "/guessNumberServlet")
 public class GuessNumberServlet extends HttpServlet {
 
-    public void doGet(HttpServletRequest request, HttpServletResponse response){
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Random random = new Random();
         int randomNumber = random.nextInt(10 + 1);
 
         String number = request.getParameter("number");
 
         if (Integer.parseInt(number) == randomNumber) {
-            response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-            response.setHeader("Location", "http://localhost:8080/Gradle___fr_eni___guessTheNumber_1_0_SNAPSHOT_war/win.html");
+            response.sendRedirect("http://localhost:8080/Gradle___fr_eni___guessTheNumber_1_0_SNAPSHOT_war/win.html");
         } else {
-            response.setStatus(HttpServletResponse.SC_MOVED_TEMPORARILY);
-            response.setHeader("Location", "http://localhost:8080/Gradle___fr_eni___guessTheNumber_1_0_SNAPSHOT_war/fail.html");
+            response.sendRedirect("http://localhost:8080/Gradle___fr_eni___guessTheNumber_1_0_SNAPSHOT_war/fail.html");
         }
     }
 
