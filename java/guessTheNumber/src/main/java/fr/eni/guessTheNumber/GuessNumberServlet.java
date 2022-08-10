8e75b810f644813a8b6d58d5a91c5aaf95924770
afkus3r <sheila.skalli@gmail.com>
2022-08-10 15:00:16 +0200

    Make max and min values editable

--- java/guessTheNumber/src/main/java/fr/eni/guessTheNumber/GuessNumberServlet.java
+++ java/guessTheNumber/src/main/java/fr/eni/guessTheNumber/GuessNumberServlet.java
@@ -6,12 +6,20 @@ import java.util.Random;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 
-@WebServlet(name = "guessNumberServlet", value = "/guessNumberServlet")
+@WebServlet(name = "guessNumberServlet",
+        urlPatterns = "/guessNumberServlet",
+        initParams =
+                {
+                        @WebInitParam(description = "Random number's max value", name = "MAX_VALUE", value = "10"),
+                        @WebInitParam(description = "Random number's min value", name = "MIN_VALUE", value = "0")
+                }
+)
+
 public class GuessNumberServlet extends HttpServlet {
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         Random random = new Random();
-        int randomNumber = random.nextInt(10 + 1);
+        int randomNumber = random.nextInt(Integer.parseInt(this.getInitParameter("MAX_VALUE")) + 1 - Integer.parseInt(this.getInitParameter("MIN_VALUE"))) + Integer.parseInt(this.getInitParameter("MIN_VALUE"));
 
         String number = request.getParameter("number");
 
