5981fcb604464f6c41d086fffb052d92947d5fa9
afkus3r <sheila.skalli@gmail.com>
2022-08-11 23:20:40 +0200

    Delete demo

--- java/demo/src/main/java/fr/eni/demo/demo/DemoServlet2.java
+++
@@ -1,9 +0,0 @@
-package fr.eni.demo.demo;
-
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-
-@WebServlet("/modules/module2/DemoServlet2")
-public class DemoServlet2 extends HttpServlet {
-
-}
