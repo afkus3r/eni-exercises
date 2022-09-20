cc87fb0c9dbe08e39d304877a9129d90dbc68818
afkus3r <sheila.skalli@gmail.com>
2022-09-20 23:42:44 +0200

    Start donkeyType project

---
+++ frontend/javascript/donkeyType/script.js
@@ -0,0 +1,38 @@
+const textArea = document.querySelector("textarea");
+const speed = document.querySelector(".speed");
+const average = document.querySelector(".average");
+let timing = 5000;
+let charCount = 0;
+let currentSpeed = 0;
+let averageSpeed = 0;
+
+textArea.addEventListener("focus", () => {
+    console.log("coucou");
+    do {
+        if (charCount === 0) {
+            setTimeout(() => {
+                currentSpeed = calculateSpeed(calculateChar(), timing);
+                speed.appendChild(currentSpeed.cloneNode());
+                average.appendChild(currentSpeed.cloneNode());
+                charCount = calculateChar();
+            }, timing);
+        } else {
+            setTimeout(() => {
+                let newCharNum = calculateChar() - charCount;
+                currentSpeed = calculateSpeed(newCharNum, timing);
+                averageSpeed = average.firstChild + currentSpeed / 2;
+                speed.appendChild(currentSpeed.cloneNode());
+                average.appendChild(averageSpeed.cloneNode());
+                charCount = calculateChar();
+            }, timing);
+        }
+    } while (charCount < 1000);
+})
+
+function calculateChar(text) {
+    return text.split("").length;
+}
+
+function calculateSpeed(charNumber, milliseconds) {
+    return charNumber * 60000 / milliseconds;
+}
