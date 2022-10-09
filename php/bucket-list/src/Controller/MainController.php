b9163a80fa048b905709e6300eb532c0951133e4
afkus3r <sheila.skalli@gmail.com>
2022-10-09 15:02:36 +0200

    Add about us page and route

--- php/bucket-list/src/Controller/MainController.php
+++ php/bucket-list/src/Controller/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
@@ -10,15 +11,16 @@ class MainController extends AbstractController
     /**
      * @Route("/", name="main_index")
      */
-    public function index() {
+    public function index(): Response
+    {
         return $this->render('main/index.html.twig');
     }
 
     /**
-     * @Route("/home", name="main_home")
+     * @Route("/about-us", name="main_aboutUs")
      */
-    public function home()
+    public function aboutUs(): Response
     {
-        return $this->render('main/home.html.twig');
+        return $this->render('main/aboutUs.html.twig');
     }
 }
