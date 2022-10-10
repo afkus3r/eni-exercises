356b873cc8310f891651836ab5be3394fa0b4e01
afkus3r <sheila.skalli@gmail.com>
2022-10-10 10:52:21 +0200

    Display wish list and wish details

--- php/bucket-list/src/Controller/WishController.php
+++ php/bucket-list/src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,14 +10,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/wish-list', name: 'wish_list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig');
+        $wishes = $wishRepository->findBy([], ['id' => 'DESC']);
+
+        return $this->render('wish/list.html.twig', ["wishes" => $wishes]);
     }
 
     #[Route('/details/{id}', name: 'wish_details')]
-    public function details(int $id): Response
+    public function details(int $id, WishRepository $wishRepository): Response
     {
-        //todo : search for wish in db
+        $wish = $wishRepository->find($id);
+
+        return $this->render('wish/details.html.twig', ["wish" => $wish]);
     }
 }
